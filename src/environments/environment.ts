30d6462e39d51d66eb9e9f14570ca679af2a90b4
Laszlo Simon <simon.laszlo@vanderlande.com>
2025-10-17 08:44:29 +0200

    fix: environmet issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
+import json from './environment.json';
+
 export interface Environment {
     readonly production: boolean;
     readonly contactFormAccessKey: string;
 }
 
 export const environment: Environment = {
-    production: import.meta.env.PROD,
-    contactFormAccessKey:
-        import.meta.env.NG_APP_CONTACT_FORM_ACCESS_KEY ??
-        import.meta.env.VITE_CONTACT_FORM_ACCESS_KEY ??
-        '',
+    production: json.production,
+    contactFormAccessKey: json.contactFormAccessKey,
 };
